7c98f0edbe812b4656217fb62583a78b1315715a
bootstraponline <code@bootstraponline.com>
2015-10-14 16:12:30 -0400

    Release 3.0.3

--- lib/rspec_junit/version.rb
+++ lib/rspec_junit/version.rb
@@ -1,4 +1,4 @@
 module RspecJunit
-  VERSION = '3.0.2' unless defined? ::RspecJunit::VERSION
+  VERSION = '3.0.3' unless defined? ::RspecJunit::VERSION
   DATE    = '2015-10-14' unless defined? ::RspecJunit::DATE
 end
