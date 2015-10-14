1076dac8d97eb75eca08a10f46622865eae42985
bootstraponline <code@bootstraponline.com>
2015-10-14 15:49:09 -0400

    Require rspec_junit in env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,10 +1,10 @@
 require 'simplecov'
 require 'coveralls'
 
-# Omit coveralls formatter since we're merging suite results via a Rake task
+# Omit coveralls formatter since we're merging suite results via a Thor task
 # https://coveralls.zendesk.com/hc/en-us/articles/201769485-Ruby-Rails
 SimpleCov.start { add_filter 'spec/' }
 
 require 'aruba/cucumber'
 require 'nokogiri'
-
+require_relative '../../lib/rspec_junit'
