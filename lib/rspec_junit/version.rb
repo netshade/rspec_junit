5d3d72675757d0824f531c3f4074c972522edad8
bootstraponline <code@bootstraponline.com>
2015-10-16 16:19:16 -0400

    Release 4.0.1

--- lib/rspec_junit/version.rb
+++ lib/rspec_junit/version.rb
@@ -1,4 +1,4 @@
 module RspecJunit
-  VERSION = '4.0.0' unless defined? ::RspecJunit::VERSION
+  VERSION = '4.0.1' unless defined? ::RspecJunit::VERSION
   DATE    = '2015-10-16' unless defined? ::RspecJunit::DATE
 end
